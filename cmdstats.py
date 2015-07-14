a2fab40b69b199ef03fa74523781c2998a13248f
JackeyGao <gaojunqi@outlook.com>
2015-07-14 20:04:03 +0800

    first commit

---
+++ cmdstats.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+'''
+File Name: history.py
+Author: JackeyGao
+mail: junqi.gao@shuyun.com
+Created Time: 2015年07月12日 星期日 20时45分02秒
+'''
+from __future__ import division
+import commands
+import argparse
+import os
+import sys
+import time
+import traceback
+
+
+__version__ = '0.1'
+
+class ParseHistoryFile(object):
+
+    def __init__(self, filepath):
+        self.filepath = filepath
+
+    def _parse_line(self, line):
+        ob = {}
+        return ob
+
+    def validate(self):
+        return True
+
+    def parse_lines(self):
+        obs = []
+        if not self.validate():
+            return obs
+
+        with open(self.filepath, 'r') as f:
+            for line in f.readlines():
+                try:
+                    ob = self._parse_line(line.strip())
+                    obs.append(ob)
+                except Exception as e:
+                    continue
+        return obs
+
+class ParseZshHistoryFile(ParseHistoryFile):
+
+    def validate(self):
+        if 'zsh' in self.filepath:
+            return True
+        return False
+
+    def _parse_line(self, line):
+        ob = {}
+        status = line.split(';')[0]
+        command_full = ';'.join(line.split(';')[1:])
+        ob["command"] = command_full.split(' ')[0]
+        ob["run_time"] = status.split(':')[1]
+        ob["status"] = status.split(':')[2]
+        return ob
+
+
+class ParseBashHistoryFile(ParseHistoryFile):
+
+    def validate(self):
+        if 'bash' in self.filepath:
+            return True
+        return False
+    
+    def _parse_line(self, line):
+        ob = {}
+        ob["command"] = line.strip()
+        return ob
+
+parses = [ParseZshHistoryFile, ParseBashHistoryFile]
+
+def sum(obs):
+    sum_dict = {}
+    for ob in obs:
+        if ob["command"] in sum_dict:
+            sum_dict[ob["command"]] += 1
+        else:
+            sum_dict[ob["command"]] = 1
+    else:
+        return sum_dict
+
+def stats(obs, limit=20):
+    count = sum(obs)
+    dict = sorted(count.iteritems(), key=lambda d:d[1], reverse = True)
+
+    shows = dict[0:limit]
+    
+    number = 0
+    data = []
+    for i in shows:
+        number += 1
+        data.append((number, i[1], round(i[1] / len(obs) * 100, 5), i[0] )) 
+    return data
+
+def show(data):
+    #message = "No\tCount\tPercentage\tCommand\n"
+    message = ""
+    for n, c, b, cmd in data:
+        message += "%s\t%s\t%s%%\t%s\n" % (n, c, b, cmd)
+
+    return commands.getoutput("echo '%s' | column -t " % message)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--limit", type=int,
+            default=20,help="显示条数""[default: %(default)s]")
+
+    args = parser.parse_args()
+    limit = args.limit
+    
+    home_dir = os.environ.get("HOME", None)
+    if home_dir is None:
+        print("No $HOME variable.")
+
+    files = os.listdir(home_dir)
+    
+    obs = []
+    for file in files:
+        file = os.path.join(home_dir, file)
+        if file.endswith('history'):
+            for parse in parses:
+                p = parse(file)
+                obs += p.parse_lines()
+
+    data = stats(obs, limit=limit)
+    sys.stdout.write(show(data) + '\n')
---
+++ steup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+'''
+File Name: steup.py
+Author: JackeyGao
+mail: junqi.gao@shuyun.com
+Created Time: 2015年07月13日 星期一 13时02分21秒
+'''
+
+from setuptools import setup, find_packages
+from cmdstats import __version__ as version, __doc__ as description
+
+
+def fread(filepath):
+    with open(filepath, 'r') as f:
+        return f.read()
+
+setup(
+    name='cmdstats',
+    version=version,
+    install_requires=[
+        'argparse',
+    ],
+    zip_safe=False,
+    py_modules = ['cmdstats'],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'cmdstats = cmd_status.history:main',
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    license="MIT",
+    description=description,
+    long_description=fread('README.rst'),
+    author='JackeyGao',
+    author_email='oommmme@gmail.com',
+    url='https://github.com/jackeygao/cmdstatus',
+)
+
+
